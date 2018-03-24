f7b59ea47ab073f74dbbdbbb9842fae299e48e4e
Lara <you@example.com>
2018-03-24 19:03:22 +0300

    Add files tto run collabtm, prepare data

--- rus_tfidf.py
+++ rus_tfidf.py
@@ -32,6 +32,9 @@ vocabulary = set()
 for w in words:
 	vocabulary.update(w)
 vocabulary = list(vocabulary)
+vocabulary.sort()
+with open("./vocab.dat", 'w') as otp:
+	otp.write('\n'.join(vocabulary))
 word_index = {w: idx for idx, w in enumerate(vocabulary)}
 tfidf = TfidfVectorizer(stop_words=stopWords, tokenizer=tokenize, vocabulary=vocabulary)
 tfidf.fit(texts)
@@ -40,12 +43,17 @@ X = tfidf.transform(texts)
 # 	print (X[2, tfidf.vocabulary_[i]], i)
 
 top_10k_words = {}
-for i in range(len(texts)):
-	name = filenames[i]
-	current_vocab = set(words[i])
-	top_words = [(w, X[i, tfidf.vocabulary_[w]]) for w in current_vocab]
-	top_words.sort(key=lambda x: x[1])
-	top_10k_words[name] = top_words[-10000:]
+with open("./mult.dat", 'w') as otp:
+	for i in range(len(texts)):
+		name = filenames[i]
+		current_vocab = set(words[i])
+		top_words = [(w, X[i, tfidf.vocabulary_[w]]) for w in current_vocab]
+		top_words.sort(key=lambda x: x[1])
+		top_10k_words[name] = top_words[-10000:]
+		otp.write("{} ".format(len(words[i])))
+		otp.write(' '.join([str(p[0]) + ":" + str(p[1]) for p in top_words]))
+		otp.write('\n')
+
 
 #print(top_10k_words[filenames[0]])
 
